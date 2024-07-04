133dc66fe7e6fca193b16e85cf619bf5840acf88
gitea <admin@admin.com>
2024-07-04 09:55:36 +0000

    first commit

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+public delegate bool Promotable(Employee E);
+
+public class Employee
+{
+    public int ID { get; set; }
+    public string Name { get; set; }
+    public int Salary { get; set; }
+    public int Experience { get; set; }
+
+    // Removed static keyword from Promotion method and corrected parameter type
+    public void Promotion(List<Employee> EmployeeList, Promotable P)
+    {
+        foreach (Employee emp in EmployeeList)
+        {
+            if (P(emp)) // Use the delegate to check if the employee is promotable
+            {
+                Console.WriteLine("{0} Promoted", emp.Name);
+            }
+        }
+    }
+}
+
+public class Ali
+{
+    public static void Main()
+    {
+        List<Employee> empList = new List<Employee>();
+        empList.Add(new Employee() { ID = 1, Name = "Mary Luisse", Salary = 5000, Experience = 5 });
+        empList.Add(new Employee() { ID = 2, Name = "John Lorusso", Salary = 4000, Experience = 4 });
+        empList.Add(new Employee() { ID = 3, Name = "Ibrahim Arabaki", Salary = 6000, Experience = 6 });
+        empList.Add(new Employee() { ID = 4, Name = "Spendthrift", Salary = 3000, Experience = 3 });
+
+        Employee E = new Employee();
+
+        E.Promotion(empList, emp => emp.Experience >= 5);  // Lambda Expression with Deligate
+    }
+}
